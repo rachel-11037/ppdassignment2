00a66358d5e32836d849f3937055d2481c298a44
23AHeavey <152389593+23AHeavey@users.noreply.github.com>
2023-12-04 12:48:14 +0000

    Coded Mass Class file

--- Python Files/mass.py
+++ Python Files/mass.py
@@ -1,4 +1,30 @@
 # Adam Heavey - Class: Mass
 
 class Mass:
-    pass
+    def __init__(self, mass):
+        self.mass = mass
+
+    @property
+    def mass(self):
+        return self._mass
+
+    @mass.setter
+    def mass(self, new):
+        self._mass = new
+
+    # Kilogrammes to Stone
+    def convert_kg_to_st(self):
+        conversion = (self.mass / 6.35)
+        return conversion
+
+    # Stone to Kilogrammes
+    def convert_st_to_kg(self):
+        conversion = (self.mass * 6.35)
+        return conversion
+
+
+if __name__ == "__main__":
+    example1 = Mass(10)
+    print(f"{example1.mass} kilogrammes converted to stone is {example1.convert_kg_to_st()}")
+    print(f"{example1.mass} stone converted to kilogrammes is {example1.convert_st_to_kg()}")
+
