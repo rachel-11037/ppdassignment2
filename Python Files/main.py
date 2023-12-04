0a6984bfec6f218b597e9ac12a61053d3c2cbaf2
Rachel Gillespie <146000008+rachel-11037@users.noreply.github.com>
2023-12-04 14:54:29 +0000

    Update main.py
    Inner functions exist thankfully so hopefully this'll be ok

--- Python Files/main.py
+++ Python Files/main.py
@@ -16,7 +16,33 @@ def menu():
     print("+ 6: Quit                           +")
     print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 
+def distance():
+    def convert_miles():
+        miles = int(input("Enter, in miles, your measurement:"))
+        miles = Distance(miles)
+        result = miles.convert_miles2km()
+        print(f"{miles.distance} miles = {result}km.")
 
+    def convert_km():
+        km = int(input("Enter, in kilometres, your measurement:"))
+        km = Distance(km)
+        result = km.convert_km2miles()
+        print(f"{km.distance}km = {result} miles.")
+
+    
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("+ Distance Conversion               +")
+    print("+ 1. Convert miles to kilometres    +")
+    print("+ 2. Convert kilometres to miles    +")
+    print("+ 3. Back to main menu              +")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    user_choice = int(input("Select your choice:"))
+    while user_choice != 3:
+        if user_choice == 1:
+            convert_miles()
+        elif user_choice == 2:
+            convert_km()
+    menu()
 def main():
     menu()
 
