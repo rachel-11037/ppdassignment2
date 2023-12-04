b3155634ed2e275fbcd73ddffecd677527d4509f
23AHeavey <152389593+23AHeavey@users.noreply.github.com>
2023-12-04 12:39:13 +0000

    Coded Area Class file

--- Python Files/area.py
+++ Python Files/area.py
@@ -1,4 +1,28 @@
 # Adam Heavey - Class: Area
 
 class Area:
-    pass
+    def __init__(self, area):
+        self.area = area
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, new):
+        self._area = new
+
+    # Acres to Hectares
+    def convert_acr_to_hect(self):
+        conversion = (self.area / 2.471)
+        return conversion
+
+    # Hectares to Acres
+    def convert_hect_to_acr(self):
+        conversion = (self.area * 2.471)
+        return conversion
+
+if __name__ == "__main__":
+    example1 = Area(100)
+    print(f"{example1.area} hectares converted to acres is {example1.convert_hect_to_acr()}")
+    print(f"{example1.area} hectares converted to acres is {example1.convert_hect_to_acr()}")
