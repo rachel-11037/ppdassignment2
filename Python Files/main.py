185b0c76b9820ca170d8b7c011b374b60f353e73
Rachel Gillespie <146000008+rachel-11037@users.noreply.github.com>
2023-12-04 14:42:21 +0000

    Create main.py
    Sorry I didn't realise I didn't have this one checked but yeah I hope I can find an efficient way to have multiple functions as I don't want to compromise performance

---
+++ Python Files/main.py
@@ -0,0 +1,24 @@
+from distance import Distance
+from temperature import Temperature
+from volume import Volume
+from mass import Mass
+from area import Area
+
+
+def menu():
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("+ Welcome to the conversion tool!   +")
+    print("+ 1: Distance                       +")
+    print("+ 2: Temperature                    +")
+    print("+ 3: Volume                         +")
+    print("+ 4: Mass                           +")
+    print("+ 5: Area                           +")
+    print("+ 6: Quit                           +")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+
+
+def main():
+    menu()
+
+
+main()
